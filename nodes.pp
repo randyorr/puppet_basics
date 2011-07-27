3a7374d8057a40ce3ac325c8f44615f4e9baec02
root <root@puppet.(none)>
2011-07-27 16:06:08 -0400

    all updates

--- nodes.pp
+++
@@ -1,9 +0,0 @@
-node 'ip-10-112-234-183.ec2.internal' {
-	file { "/tmp/randy.txt":
-			path => "/tmp/randy.txt",
-			owner => root, group => root, mode => 440,
-			content => "hello, just testing going to a specific node"
-	}
-}
-
-	
